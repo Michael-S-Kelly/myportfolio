ac970a3d801388e2b598ab7521914a78dd40e0f1
Michael Kelly <mike.s.kelly@icloud.com>
2019-06-06 20:38:49 -0700

    Created Register, Login, and Logout functionality

---
+++ MyPortfolio/MyPortfolio/Data/MyPortfolioUserDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MyPortfolio.Models;
+
+namespace MyPortfolio.Data
+{
+    public class MyPortfolioUserDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+
+        public MyPortfolioUserDbContext(DbContextOptions<MyPortfolioUserDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ MyPortfolio/MyPortfolio/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPortfolio.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(20, ErrorMessage = "The {0} must be at least {2}, and a max of {1} characters long", MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The passwords do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
